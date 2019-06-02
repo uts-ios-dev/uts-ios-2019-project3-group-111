3e8a320ce2174e73a2b1b169064a406e554ba8a4
Viola Wang <cello1007@gmail.com>
2019-06-02 14:47:44 +1000

    Add background animation to login view controller

--- GoDefine/GoDefine/ViewControllers/Login+ViewController.swift
+++ GoDefine/GoDefine/ViewControllers/Login+ViewController.swift
@@ -13,10 +13,12 @@ class LoginViewController: UIViewController {
     override var prefersStatusBarHidden: Bool { return true }
     override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation { return .fade }
     
+    @IBOutlet weak var backgroundView: UIImageView?
+    @IBOutlet weak var buttonView: UIView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -25,6 +27,25 @@ class LoginViewController: UIViewController {
         if let _ = user.name {
             performSegue(withIdentifier: "EnterHomeDirect", sender: nil)
         }
+        
+        if backgroundView?.layer.animation(forKey: "shift") == nil {
+            startBackgroundAnimation()
+        }
+    }
+    
+    private func startBackgroundAnimation() {
+        guard let backgroundView = backgroundView else { return }
+        let rightLength = backgroundView.frame.maxX - view.frame.maxX
+        let leftLength = view.frame.minX - backgroundView.frame.minX
+        
+        let shiftAnimation = CAKeyframeAnimation(keyPath: "position.x")
+        shiftAnimation.values = [0, -leftLength, 0, rightLength, 0]
+        shiftAnimation.timingFunction = .linear
+        shiftAnimation.duration = 100.0
+        shiftAnimation.isAdditive = true
+        shiftAnimation.repeatCount = Float.infinity
+        
+        backgroundView.layer.add(shiftAnimation, forKey: "shift")
     }
 
 }
